b9635fbf636d55bbeeb9663b9aba396f97b444b7
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-24 23:11:35 +0530

    feat: modified app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeroSection from './components/herosection';
+import Navbar from './components/nav-bar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Navbar />
+      <HeroSection />
+      {/* Rest of your app components */}
     </div>
   );
 }
