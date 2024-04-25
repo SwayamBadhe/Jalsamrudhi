4976e418fadec2c6c2d1ceaaa2acf3ac85a3384b
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-25 17:02:30 +0530

    feat: Created aboutUs component

---
+++ frontend/src/components/aboutUs.tsx
@@ -0,0 +1,30 @@
+const AboutUs = () => {
+  return (
+    <div className="p-7">
+      <div className="bg-slate-100 rounded-md shadow-md mx-2 p-4">
+        <div className="text-3xl font-bold text-center">
+          Jal Samruddha Nashik
+        </div>
+        <div className="grid grid-cols-2 gap-16">
+          <div className="text-lg mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolor
+            accusamus iste voluptate sed quisquam placeat. Sed recusandae quidem
+            praesentium perferendis voluptas unde id officia? Quisquam fugiat
+            numquam, explicabo eius vitae labore repellendus consequuntur cumque
+            adipisci qui non magnam neque culpa illo, perferendis laudantium
+            praesentium dolore doloremque ea, molestiae repudiandae!
+          </div>
+          <div className="text-lg mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi unde
+            ab voluptate repellendus eligendi, delectus ut. Et illo totam
+            aspernatur praesentium quas, reiciendis atque itaque voluptate illum
+            odio magni officiis voluptas eos mollitia odit eligendi neque?
+            Explicabo magni, neque consequatur laudantium dignissimos numquam
+            quam quos minus quidem id quaerat voluptatum!{' '}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AboutUs;
