0d27daf37e9533ab99035cb6dd3cc2f85d06d93a
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-25 17:06:05 +0530

    feat: Added AboutUs component to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import AboutUs from './components/aboutUs';
 import HeroSection from './components/herosection';
 import MediaGallery from './components/mediaGallery';
 import Navbar from './components/nav-bar';
@@ -9,6 +10,7 @@ function App() {
       <HeroSection />
       {/* Rest of your app components */}
       <MediaGallery />
+      <AboutUs />
     </div>
   );
 }
