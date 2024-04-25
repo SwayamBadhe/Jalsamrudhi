9b0a3fd973867d22073461d3cb64bcd6667cdda4
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-25 10:57:36 +0530

    feat: Add MediaGallery component to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import HeroSection from './components/herosection';
+// import HeroSection from './components/herosection';
+import MediaGallery from './components/mediaGallery';
 import Navbar from './components/nav-bar';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <HeroSection />
+      {/* <HeroSection /> */}
       {/* Rest of your app components */}
+      <MediaGallery />
     </div>
   );
 }
 
 export default App;
-
-dahjsvdjhasd;
