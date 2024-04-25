f12001a71540ec84295ca19b9861b445a39bd882
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-25 10:59:20 +0530

    feat: Add fetch-media endpoint to server file

--- ts-payload/src/server.ts
+++ ts-payload/src/server.ts
@@ -1,13 +1,35 @@
-import express from 'express'
-import payload from 'payload'
+import express from 'express';
+import payload from 'payload';
 
-require('dotenv').config()
-const app = express()
+const cors = require('cors');
+const axios = require('axios');
+require('dotenv').config();
+const app = express();
+app.use(cors());
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
-  res.redirect('/admin')
-})
+  res.redirect('/admin');
+});
+
+app.get('/fetch-media', async (req, res) => {
+  const result = await payload.find({
+    collection: 'media',
+  });
+
+  const media = await result.docs;
+
+  const media_card = media.map((item) => ({
+    id: item.id,
+    alt: item.alt,
+    filename: item.filename,
+    cardImageUrl: item.sizes.card.url, // URL of the card-sized image
+    createdAt: item.createdAt,
+    updatedAt: item.updatedAt,
+  }));
+
+  res.json(media_card);
+});
 
 const start = async () => {
   // Initialize Payload
@@ -15,13 +37,13 @@ const start = async () => {
     secret: process.env.PAYLOAD_SECRET,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
   // Add your own express routes here
 
-  app.listen(3000)
-}
+  app.listen(5500);
+};
 
-start()
+start();
