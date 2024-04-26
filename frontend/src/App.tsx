e25cbfc23b321be48de5745b714e958cebe9dc74
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-26 11:36:08 +0530

    refactor: Update App component structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
+import { useLocation } from 'react-router-dom';
 import AboutUs from './components/aboutUs';
 import HeroSection from './components/herosection';
 import MediaGallery from './components/mediaGallery';
 import Navbar from './components/nav-bar';
+import AppRoutes from './routes/routes';
+import { Grid } from '@mui/material';
+import Footer from './components/footer';
 
 function App() {
+  const location = useLocation();
+
+  const showHeroSection = location.pathname !== '/donate';
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      {/* Rest of your app components */}
-      <MediaGallery />
-      <AboutUs />
-    </div>
+    <Grid>
+      <div>
+        <Navbar />
+        {showHeroSection && <HeroSection />}
+        <AppRoutes />
+        {/* Rest of your app components */}
+        <MediaGallery />
+        <AboutUs />
+        <Footer />
+      </div>
+    </Grid>
   );
 }
 
