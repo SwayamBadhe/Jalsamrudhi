c405170f0d2f89a92c8e156c0c4b75d26802e8eb
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-28 00:12:57 +0530

    refactor: add stying

--- frontend/src/components/mediaGallery.tsx
+++ frontend/src/components/mediaGallery.tsx
@@ -33,11 +33,15 @@ const MediaGallery = () => {
   };
 
   return (
-    <div className="py-6 ml-6">
-      <div className="grid grid-cols-3 gap-6">
+    <div className="py-6 ml-6 mt-5 bg-slate-50 bg-opacity-85 rounded-3xl shadow-md">
+      <div className="grid grid-cols-3 gap-6 p-5">
         {media.map((item) => (
-          <div key={item.id} className="relative">
-            <img src={getImageUrl(item.imageUrl)} alt={item.alt} />
+          <div key={item.id} className="relative overflow-hidden rounded-lg">
+            <img
+              src={getImageUrl(item.imageUrl)}
+              alt={item.alt}
+              className="w-full h-auto transform transition duration-300 hover:scale-105"
+            />
           </div>
         ))}
       </div>
