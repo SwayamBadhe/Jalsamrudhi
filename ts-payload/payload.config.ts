ca22b7ff206ec5e6bd7d05c9a198d1952b174269
Swayam Badhe <swayampbadhe@gmail.com>
2024-04-28 18:00:08 +0530

    fix: create a copy of payload.config

---
+++ ts-payload/payload.config.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+
+import { payloadCloud } from '@payloadcms/plugin-cloud';
+import { mongooseAdapter } from '@payloadcms/db-mongodb';
+import { webpackBundler } from '@payloadcms/bundler-webpack';
+import { slateEditor } from '@payloadcms/richtext-slate';
+import { buildConfig } from 'payload/config';
+
+import Users from './src/collections/Users';
+import { Media } from './src/collections/Media';
+import { Media_Gallery } from './src/collections/MediaPhotoGallery';
+import { AboutUs } from './src/collections/About_us';
+import { JSN } from './src/collections/JSN';
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: slateEditor({}),
+  collections: [Users, Media, Media_Gallery, AboutUs, JSN],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  plugins: [payloadCloud()],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+});
